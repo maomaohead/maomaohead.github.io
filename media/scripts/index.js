1be11036d7b5a99f873afc4e207c8158ae4d4b66
MaoMaoHead <377312239@qq.com>
2023-12-11 15:20:44 +0800

    update from gridea: 2023-12-11 15:20:44

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -1,10 +1,1 @@
-console.log('Hi welcome to minloha.cn');
-$(document).ready(function(){
-    $(".list_right").hide();
-    $("#right_list").click(function(){
-        $(".list_right").fadeToggle(500);
-    });
-    $("#right_list").blur(function(){
-        $(".list_right").fadeOut(500);
-    });
-});
+console.log('Hello Gridea')
